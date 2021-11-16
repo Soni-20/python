c1af33422534a0c7640b11bf014fdbdc1b065481
Soni-20 <93289594+Soni-20@users.noreply.github.com>
2021-11-16 19:23:15 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,9 @@
+
+print("enter the year")
+year=int(input())
+if((year%400==0) or
+   (year%100!=0) and
+   (year%4==0)):
+    print("given year is a leap year")
+else:
+        print("given year is not a leap year")
