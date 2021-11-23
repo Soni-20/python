a4657b5307421b3df1027c258d146c5d5425099b
Soni-20 <93289594+Soni-20@users.noreply.github.com>
2021-11-23 19:57:34 +0530

    Add files via upload

---
+++ integers greater 100.py
@@ -0,0 +1,9 @@
+intlist1 = print("enter the integers")
+list1=input()
+  
+# iterating each number in list
+for over in list1:
+      
+    # checking condition
+    if over >= 100:
+       print("over", over)
