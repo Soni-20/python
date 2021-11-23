6adfa1899a472e679439efda9c94cf9169b757f8
Soni-20 <93289594+Soni-20@users.noreply.github.com>
2021-11-23 19:58:59 +0530

    Add files via upload

--- python25.py
+++ python25.py
@@ -1,3 +1,5 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+s="python"
+s1=s[0]
+s2=s[-1]
+s3=[1:len(s):-1]
+print("replaced string is"s2+s3+s1)
