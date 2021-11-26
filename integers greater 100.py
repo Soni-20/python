2597a34c46a7b6ece4e04ca7c477c8c721ccbcf7
Soni-20 <93289594+Soni-20@users.noreply.github.com>
2021-11-27 04:53:49 +0530

    Delete integers greater 100.py

--- integers greater 100.py
+++
@@ -1,9 +0,0 @@
-intlist1 = print("enter the integers")
-list1=input()
-  
-# iterating each number in list
-for over in list1:
-      
-    # checking condition
-    if over >= 100:
-       print("over", over)
