79bed4bb92b90ba052811d3a1344ba13c3d51349
Soni-20 <93289594+Soni-20@users.noreply.github.com>
2021-11-28 18:00:59 +0530

    Add files via upload

---
+++ ascen&descen.py
@@ -0,0 +1,3 @@
+moth={"jan":"31","feb":"28","march":"31","april":"30","may":"31","june":"30","july":"31","aug":"31","sept":"30","oct":"31","nov":"30","dec":"31"}
+x=month.keys()
+print('sorted by keys".sort (x))
